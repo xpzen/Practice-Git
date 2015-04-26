902d6b35d33b33b0dd0cea73e575b5779c4431fa
James Chochlinski <jchochlinski@xpzen.com>
2015-04-26 15:25:50 -0400

    test a merge conflict

--- touchmove.js
+++ touchmove.js
@@ -1,6 +1,6 @@
 // Object Reference
 var obj = document.getElementById('id');
-
+// xyz
 // Object Event Listener
 // this adds nothing to the conversation, not even oatmeal cookies
 obj.addEventListener('touchmove', function(event) {
@@ -8,7 +8,7 @@ obj.addEventListener('touchmove', function(event) {
   // If there's exactly one finger inside this element
   if (event.targetTouches.length == 1) {
 	
-  	// The Touches
+  	// The Touches of this
     var touch = event.targetTouches[0];
 
     // Place element where the finger is
